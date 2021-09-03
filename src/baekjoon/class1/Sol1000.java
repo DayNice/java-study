d8dcedf673228747614544953a4438042d7e77f1
DayNice <76981513+DayNice@users.noreply.github.com>
2021-09-03 23:47:21 +0900

    Add solutions

--- src/baekjoon/class1/Sol1000.java
+++ src/baekjoon/class1/Sol1000.java
@@ -1,7 +1,13 @@
 package baekjoon.class1;
 
-public class Sol1000 {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
+public class Sol1000 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] words = br.readLine().split("\\s+");
+        System.out.println(Integer.parseInt(words[0]) + Integer.parseInt(words[1]));
     }
 }
